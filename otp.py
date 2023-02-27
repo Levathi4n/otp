07b385e1859e8487d74e606a52ca35dbd019916e
Oxida007 <123311208+Oxida007@users.noreply.github.com>
2023-02-27 15:45:43 +0700

    Update otp.py

--- otp.py
+++ otp.py
@@ -4,15 +4,16 @@ import random as r
 def otp():
   url = '' #your webhook discord
   otp = r.randint(50, 250) + r.randint(70,120)
+
   data = {
     "username" : "OTP Logs",
     "content" : f"{otp}"
 }
 
-  result = requests.post(url, json = data)
-  otps = input("Verification code : ")
-  if otps == otp:
-    print("Correct")
-    pass
-  else:
-    exit()
+result = requests.post(url, json = data)
+otps = input("Verification code : ")
+if otps == otp:
+  print("Correct")
+  pass
+else:
+  exit()
